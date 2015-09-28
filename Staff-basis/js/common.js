7cd87a3e142fccddabed34fa0047514329e6449f
semenz <semen.z@kodeks.ru>
2015-09-28 16:37:25 +0300

    Шрифты и дополнения

---
+++ Staff-basis/js/common.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('.theme-1').mouseover(function(event){
+		$('.button-theme1').toggleClass('hide');
+	});
+	$('.theme-1').mouseout(function(event){
+		$('.button-theme1').toggleClass('hide');
+	});
+
+	$('.theme-2').mouseover(function(event){
+		$('.button-theme2').toggleClass('hide');
+	});
+	$('.theme-2').mouseout(function(event){
+		$('.button-theme2').toggleClass('hide');
+	});
+
+	$('.theme-3').mouseover(function(event){
+		$('.button-theme3').toggleClass('hide');
+	});
+	$('.theme-3').mouseout(function(event){
+		$('.button-theme3').toggleClass('hide');
+	});
+
+	$('.theme-4').mouseover(function(event){
+		$('.button-theme4').toggleClass('hide');
+	});
+	$('.theme-4').mouseout(function(event){
+		$('.button-theme4').toggleClass('hide');
+	});
+});
