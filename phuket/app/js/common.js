d759410489aabd99b19196aad6f056e0622d70e0
semenz <semen.z@kodeks.ru>
2016-02-18 13:29:15 +0300

    add owl-slider

--- phuket/app/js/common.js
+++ phuket/app/js/common.js
@@ -36,5 +36,22 @@ $(function() {
 	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+	// owl-slider
+	$("#owl-demo").owlCarousel({
+
+			navigation : true, // Show next and prev buttons
+			slideSpeed : 300,
+			paginationSpeed : 400,
+			singleItem:true
+
+			// "singleItem:true" is a shortcut for:
+			// items : 1, 
+			// itemsDesktop : false,
+			// itemsDesktopSmall : false,
+			// itemsTablet: false,
+			// itemsMobile : false
+
+	});
 
 });
