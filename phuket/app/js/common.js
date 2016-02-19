b3cf3528773f0362402e4136a59601ef499fddea
Simonzzzz <semen@codedesign.ru>
2016-02-19 03:04:16 +0300

    add tabs

--- phuket/app/js/common.js
+++ phuket/app/js/common.js
@@ -36,22 +36,31 @@ $(function() {
 	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
+
 	// owl-slider
 	$("#owl-demo").owlCarousel({
 
 			navigation : true, // Show next and prev buttons
 			slideSpeed : 300,
 			paginationSpeed : 400,
-			singleItem:true
+			singleItem: true,
+			pagination: false,
+			navigationText : ["",""],
 
 			// "singleItem:true" is a shortcut for:
-			// items : 1, 
+			// items : 1,
 			// itemsDesktop : false,
 			// itemsDesktopSmall : false,
 			// itemsTablet: false,
 			// itemsMobile : false
 
 	});
+
+	// tabs
+	$('.__js-tabs-i').click(function() {
+		$('.__js-tabs-i').removeClass('__active').eq($(this).index()).addClass('__active');
+		$(".__js-tabs-cnt").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass('__active');
+	$(".__js-tabs-cnt").eq(0).show();
 
 });
